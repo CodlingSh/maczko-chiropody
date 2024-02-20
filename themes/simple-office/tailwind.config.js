45b6479b8af4dec08f9c8d82b4a9fb6015fa71f4
Sheldon Codling <codlinsh@gmail.com>
2024-02-20 03:21:24 -0500

    Fixed content parameter to add the content thats outside of the theme

--- themes/simple-office/tailwind.config.js
+++ themes/simple-office/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./content/**/*.{html,js}", "./layouts/**/*.{html,js}", "./static/js/*.js"],
+  content: ["./content/**/*.{html,js}", "./layouts/**/*.{html,js}", "./static/js/*.js", "../../layouts/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
